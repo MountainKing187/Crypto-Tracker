dfea845883cde07f2be98bd14f8f57f515c30434
nyest11 <nyest@aaa.com>
2025-07-04 19:48:16 -0400

    añadido codigo en config_loader.py

--- app/utils/config_loader.py
+++ app/utils/config_loader.py
@@ -1,1 +1,19 @@
+import os
+from dotenv import load_dotenv
 
+def load_config():
+    load_dotenv()  # Cargar variables de entorno desde .env
+    
+    class Config:
+        MONGODB_URI = os.getenv('MONGODB_URI')
+        MONGO_DB_NAME = os.getenv('MONGO_DB_NAME', 'crypto_data')
+        ALCHEMY_API_KEY = os.getenv('ALCHEMY_API_KEY')
+        SECRET_KEY = os.getenv('FLASK_SECRET_KEY', 'supersecretkey')
+        
+        @property
+        def ETHEREUM_NODE_WS(self):
+            if self.ALCHEMY_API_KEY:
+                return f"wss://eth-mainnet.g.alchemy.com/v2/{self.ALCHEMY_API_KEY}"
+            return None
+    
+    return Config()
