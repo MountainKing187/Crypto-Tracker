013e92ddbcb4186d3b3c2376bf280f9eda35ea51
nyest11 <nyest@aaa.com>
2025-07-11 19:59:10 -0400

    print en gemini agent

--- app/ia_agent/analisis_handler.py
+++ app/ia_agent/analisis_handler.py
@@ -3,11 +3,12 @@ from app.ia_agent.notification_service import NotificationService
 from app.ia_agent.gemini_agent import GeminiAgent
 
 class AnalisisHandler:
-    def run(self, interval_seconds=3600):
+    def run(self, interval_seconds=600):
         print(f"Iniciando analisis ...")
-        servicioNotificacion = NotificationService()
-        servicioNotificacion.send_notification_to_all("Actualizacion de analisis", "Se ha actualizado el analisis con IA")
-        AgenteIA = GeminiAgent()
-        AgenteIA.analizar_datos_gemini()
-        time.sleep(interval_seconds)
+        while True:
+            servicioNotificacion = NotificationService()
+            servicioNotificacion.send_notification_to_all("Actualizacion de analisis", "Se ha actualizado el analisis con IA")
+            AgenteIA = GeminiAgent()
+            AgenteIA.analizar_datos_gemini()
+            time.sleep(interval_seconds)
          
